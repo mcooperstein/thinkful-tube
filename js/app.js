524c07ab3b84a2a51801ba4ad57e30c5dfb024c6
Marc Cooperstein <mcooperstein@gmail.com>
2016-03-06 22:35:25 -0800

    final commit
    made thumbnails clickable links

--- js/app.js
+++ js/app.js
@@ -9,17 +9,13 @@ $(document).ready(function () {
 
 
 
-
-
-
-
-
     // STEP 2 - using the input from the user (query) make the API call to get the JSON response
     function getRequest(searchTerm) {
         $.getJSON("https://www.googleapis.com/youtube/v3/search", {
                 "part": "snippet",
                 "key": "AIzaSyAragdDZXDXqbtyG43Tv0do3z1JCxEuI_A",
-                "q": searchTerm
+                "q": searchTerm,
+                "type": "video"
             },
             function (data) {
                 // If there are no results it will just empty the list
@@ -33,9 +29,6 @@ $(document).ready(function () {
     }
 
 
-
-
-
     // STEP 3 - using the JSON response (videos), populate the relevant part of your HTML with the variable inside the JSON
     function displaySearchResults(videos) {
         var buildTheHtmlOutput = "";
@@ -43,12 +36,17 @@ $(document).ready(function () {
             // append li to ul
             console.log(video.snippet.thumbnails.medium.url);
             //concatenate the results inside the HTML variable
-            buildTheHtmlOutput += "<li><p>" + video.snippet.title + "</p><img src='" + video.snippet.thumbnails.high.url + "'/></li>";
+            buildTheHtmlOutput += "<li><p>" + video.snippet.title + "</p><a href='https://www.youtube.com/watch?v=" + video.id.videoId + "' target='_blank'><img src='" + video.snippet.thumbnails.high.url + "'/></a></li>";
 
         });
         //use the HTML output to show it in the index.html
         $("#search-results ul").html(buildTheHtmlOutput);
     }
+
+
+    // Step 4 - make the images clickable links to the youtube video
+
+
 
 
 });
